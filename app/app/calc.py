7e40573c9254ce56c58f82ba6b9dfb93f906b90d
Ryan Yu <ryan.zhenglin.yu@gmail.com>
2022-08-21 19:29:31 -0500

    Installed PostgreSQL adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.sub(6, 1)
+        self.assertEqual(res, 5)
+        
