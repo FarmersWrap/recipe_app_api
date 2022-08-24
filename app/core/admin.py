fbe59fdc1341df44017b4fe0eab031c8ba8729c8
Ryan Yu <ryan.zhenglin.yu@gmail.com>
2022-08-24 12:09:31 -0500

    Built user APIs

--- app/core/admin.py
+++ app/core/admin.py
@@ -14,6 +14,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
@@ -21,7 +22,7 @@ class UserAdmin(BaseUserAdmin):
                     'is_active',
                     'is_staff',
                     'is_superuser',
-                )
+                ),
             }
         ),
         (_('Important dates'), {'fields': ('last_login', )}),
